bdbeff8d63ee1660a3965cd666d668f0cc9f4d2a
shashank-shark <shashankreddy231@gmail.com>
2020-02-12 00:49:43 +0530

    custom panel

---
+++ fog-computing/src/custom/Panel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Panel = (props) => {
+    
+
+    return (
+        <div style={{ background: 'black'}}>
+            {props.name}
+        </div>
+    );
+
+};
+
+export default Panel;
