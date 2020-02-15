bef67bfa975323b418f17a4038d74bbcfc67940e
shashank-shark <shashankreddy231@gmail.com>
2020-02-15 21:59:02 +0530

    Added screens - Home, Analy, Serv

---
+++ fog-computing/src/screens/HomeScreen.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const HomeScreen = () => {
+
+    return (
+        <div>
+            HomeScreen
+        </div>
+    );
+};
+
+export default HomeScreen;
---
+++ fog-computing/src/screens/ServicesScreen.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const ServicesScreen = () => {
+
+    return (
+        <div>
+            ServicesScreen
+        </div>
+    );
+};
+
+export default ServicesScreen;
